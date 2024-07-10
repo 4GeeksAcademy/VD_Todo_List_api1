49364814c6a41e9c0a16ad82e290a8f7ebbbe99b
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-07-10 20:59:49 +0000

    falta modif estetica

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Input from "./input";
 import List from "./list";
 
@@ -8,16 +8,126 @@ import List from "./list";
 const Home = () => {
 	const [texto_Prueba, setTexto_Prueba] = useState([]);
 	
-	const entradaTeclado = (palabra) => {
-        setTexto_Prueba(texto_Prueba.concat(palabra));
-    };
 	
-	return (
-		<div className="text-center">
-			<Input onEnter={entradaTeclado}/>
-			<List palabras={texto_Prueba}/>
-		</div>
-	);
+	const botonEliminar = (index) => {
+		console.log(index);
+		
+		fetch('https://playground.4geeks.com/todo/todos/' + index, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			},
+		})
+			.then((response) => {
+				if (response.status === 204) {
+					obtener_Usuario_Tarea()
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data)
+				// if (data) {
+				// 	setTexto_Prueba(texto_Prueba => texto_Prueba.filter());
+				// }
+			})
+		.catch ((error) => console.log(error))
+	};
+
+const entradaTeclado = (palabra) => {
+	fetch('https://playground.4geeks.com/todo/todos/diego36', {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"label": palabra,
+			"is_done": false
+		})
+	})
+		.then((response) => {
+			console.log(response.status)
+			if (response.status == 201) {
+				obtener_Usuario_Tarea()
+			}
+			return response.json()
+		})
+		.then((data) => console.log(data))
+		.catch((error) => console.log(error))
+
+	// setTexto_Prueba(texto_Prueba.concat(palabra));
 };
 
+function editarTarea(id,tareaModificada){
+	fetch('https://playground.4geeks.com/todo/todos/' + id, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			"label": tareaModificada,
+			"is_done": false
+		})	
+	})
+		.then((response) => {
+			// console.log(response.status)
+			if (response.status === 200) {
+				obtener_Usuario_Tarea()
+			}
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data)
+			// if (data) {
+			// 	setTexto_Prueba(texto_Prueba => texto_Prueba.filter());
+			// }
+		})
+	.catch ((error) => console.log(error))
+
+}
+
+function crearUsuario() {
+	fetch('https://playground.4geeks.com/todo/users/diego36', {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		}
+
+	})
+		.then((response) => response.json())
+		.then((data) => console.log(data))
+		.catch((error) => console.log(error))
+}
+
+function obtener_Usuario_Tarea() {
+	fetch('https://playground.4geeks.com/todo/users/diego36', {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json"
+		}
+
+	})
+		.then((response) => {
+			console.log(response.status)
+			if (response.status == 404) {
+				crearUsuario()
+			}
+			return response.json()
+		})
+
+		.then((data) => setTexto_Prueba(data.todos))
+		.catch((error) => console.log(error))
+}
+
+useEffect(() => {
+	// crearUsuario()
+	obtener_Usuario_Tarea()
+}, [])
+
+return (
+	<div className="text-center">
+		<Input onEnter={entradaTeclado} />
+		<List palabras={texto_Prueba} onDelete={botonEliminar} onEdit={editarTarea}/>
+	</div>
+);
+	};
 export default Home;
--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const List = ({ palabras }) => {
-    const contador_lineas=palabras.length;
+const List = ({ palabras, onDelete, onEdit }) => {
+    console.log(palabras)
+
+    const [tareaModificada, setTareaModificada] = useState("");
+    const contador_lineas = palabras.length;
+
     return (
         <div className="container mt-1">
-            
-            <ul className="list-group mx-auto mt-0" style={{ maxWidth: '600px' }}>
-            {palabras.map((palabra, index) => (
-                    <li key={index} className="w-100 d-flex justify-content-between align-items-center border">{palabra}<button className="btn btn-danger btn-sm float-end"><i className="fa fa-trash"></i></button></li>
 
-            ))}
+            <ul className="list-group mx-auto mt-0" style={{ maxWidth: '600px' }}>
+                {palabras.map((palabra, index) => (
+                    <li key={index} className="w-100 d-flex justify-content-between align-items-center border">
+                        {palabra.label}
+                        <button className="btn btn-danger btn-sm float-end"
+                            onClick={() => onDelete(palabra.id)}>
+
+                            <i className="fa fa-trash"></i>
+                        </button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            editar
+                        </button>
+
+                        {/* <!-- Modal --> */}
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <input type="text" className="form-control" id="floatingInputGroup1" placeholder="Ingreso texto"
+                                            value={tareaModificada}
+                                            onChange={(e) => setTareaModificada(e.target.value)}
+                                        />
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={() => onEdit(palabra.id, tareaModificada)}>
+                                            editar
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </li>
+
+                ))}
                 <li className="list-group-item text-muted small text-start">{contador_lineas} item left</li>
             </ul>
         </div>
